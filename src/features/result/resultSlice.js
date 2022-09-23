b55378d2373a0791d159fb94a8b39187e64f40fa
Joachim Gonvers <joachim.gonvers@gmail.com>
2022-09-23 11:37:16 +0200

    remove initial value for result

--- src/features/result/resultSlice.js
+++ src/features/result/resultSlice.js
@@ -1,14 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const datas = {
-  data1: [{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }, { x: 4, y: 4 }],
-  data2: [{ x: 1, y: 4 }, { x: 2, y: 3 }, { x: 3, y: 2 }, { x: 4, y: 1 }],
-  data3: [{ x: 4, y: 16 }, { x: 5, y: 25 }, { x: 1, y: 1 }, { x: 3, y: 9 }],
-};
-
 export const resultSlice = createSlice({
   name: 'Result',
-  initialState: datas,
+  initialState: {},
   reducers: {
     changeResult: (state, action) => { return action.payload; },
   },
