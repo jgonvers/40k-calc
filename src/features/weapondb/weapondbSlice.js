1bb69f135c6af56251f14fc8cded9dc118cf13e4
Joachim Gonvers <joachim.gonvers@gmail.com>
2022-10-03 12:40:36 +0200

    removed test weapon from db

--- src/features/weapondb/weapondbSlice.js
+++ src/features/weapondb/weapondbSlice.js
@@ -10,7 +10,6 @@ const damageDistribution = {
 const atkDB = [
   { name: 'heavy stubber', S: 4, B: 4, AP: 0, D: 1 },
   { name: 'lascannon', S: 9, B: 1, AP: -3, D: damageDistribution.d6 },
-  { name: "really, really, really long-ass name for test, it's a really long name trust me", S: 4, B: 4, AP: 0, D: 1 }
 ];
 
 const WeaponDataBase = {};
